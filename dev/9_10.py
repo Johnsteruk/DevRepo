a585368a6ffcf99932ffa641eff69836171aa228
johnsteruk <john.campbell7@gmail.com>
2021-04-20 19:08:54 +0200

    Adding changes to 9_10 for user module test

--- dev/9_10.py
+++ dev/9_10.py
@@ -20,6 +20,21 @@ print(f"Number of people served : {channel_club.number_served}")
 #Store the classes User , Privileges , and Admin in one module. Create a sepa-
 #rate file, make an Admin instance, and call show_privileges() to show that
 #everything is working correctly.
+from user import Admin
+
+eric = Admin('eric', 'matthes', 'e_matthes', 'e_matthes@example.com', 'alaska')
+eric.describe_user()
+
+eric_privileges = [
+    'can reset passwords',
+    'can moderate discussions',
+    'can suspend accounts',
+    ]
+eric.privileges.privileges = eric_privileges
+
+print(f"\nThe admin {eric.username} has these privileges: ")
+eric.privileges.show_privileges()
+
 
 
 #9-12. Multiple Modules: Store the User class in one module, and store the
