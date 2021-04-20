6860b3cc81ba9e52518a8703f3a6f4dd4f940ba5
johnsteruk <john.campbell7@gmail.com>
2021-04-20 17:03:41 +0200

    Committing to dev branch

---
+++ dev/9_10.py
@@ -0,0 +1,18 @@
+#!/usr/env python3
+
+#9-10. Imported Restaurant: Using your latest Restaurant class, store it in a mod-
+#ule. Make a separate file that imports Restaurant . Make a Restaurant instance,
+#and call one of Restaurant ’s methods to show that the import statement is work-
+#ing properly.
+
+
+#9-11. Imported Admin: Start with your work from Exercise 9-8 (page 173).
+#Store the classes User , Privileges , and Admin in one module. Create a sepa-
+#rate file, make an Admin instance, and call show_privileges() to show that
+#everything is working correctly.
+
+
+#9-12. Multiple Modules: Store the User class in one module, and store the
+#Privileges and Admin classes in a separate module. In a separate file, create
+#an Admin instance and call show_privileges() to show that everything is still
+#working correctly.
